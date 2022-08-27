9e0c1ea3dc7e9762590bf6692ef6da716f9f52ae
juan jose duran henao <juanjoduran2003@gmail.com>
2022-08-26 22:06:58 -0500

    add class test to main

--- proyect_the_ravens/src/Main.java
+++ proyect_the_ravens/src/Main.java
@@ -1,5 +1,7 @@
 package proyect_the_ravens.src;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -11,7 +13,7 @@ public class Main {
 
         //empleados
         Empleado juan_jose = new Empleado("juan jose ", "juanjoduran2003@gmail.com", "1", "operativo");
-        Empleado linda = new Empleado("Linda Avendaño Bautista ", "lindajaz@hotmail.com", "3", "iooo");
+        Empleado linda = new Empleado("Linda Avendaño Bautista ", "lindajaz@hotmail.com", "3", "administrador");
         Empleado merly = new Empleado("Merly Tatiana Campo Guevara ", "merlycampogue@hotmail.com", "1", "operativo");
         Empleado alfredo = new Empleado("Alfredo ALonso Gil Cuello", "gilcuello@gmail.com", "2", "operativo");
         Empleado deiver = new Empleado("Deyber Antonio Jiménez Gordillo ", "deyberantonio@gmail.com", "2", "operativo");
@@ -19,12 +21,56 @@ public class Main {
         //movimientos
         MovimientoDinero primero =new MovimientoDinero("compra mc ","juanjoduran2003@gmail.com","negativo",50000);
 
+        //EMPRESA
         System.out.println(coca_cola.getDireccion());
+        coca_cola.setDireccion("colombia");
+        System.out.println(coca_cola.getDireccion());
+
         System.out.println(coca_cola.getNit());
+        coca_cola.setNit("4");
+        System.out.println(coca_cola.getNit());
+
         System.out.println(coca_cola.getNombre());
         coca_cola.setNombre("jet");
         System.out.println(coca_cola.getNombre());
 
-        System.out.println(linda.getRol());
+        System.out.println(coca_cola.getTelefono());
+        coca_cola.setTelefono("3148658092");
+        System.out.println(coca_cola.getTelefono());
+
+
+        //empleado
+        System.out.println(alfredo.getRol());
+        alfredo.setRol("admministrador");
+        System.out.println(alfredo.getRol());
+
+        System.out.println(alfredo.getCorreo());
+        alfredo.setCorreo("alfredo@strange.com");
+        System.out.println(alfredo.getCorreo());
+
+        System.out.println(alfredo.getEmpresa());
+        alfredo.setEmpresa("1");
+        System.out.println(alfredo.getEmpresa());
+
+        System.out.println(alfredo.getNombre());
+        alfredo.setNombre("alferero guzman");
+        System.out.println(alfredo.getNombre());
+
+        //movimiento dinero
+        System.out.println(primero.getConcepto());
+        primero.setConcepto("pollo");
+        System.out.println(primero.getConcepto());
+
+        System.out.println(primero.getTipo());
+        primero.setTipo("positivo");
+        System.out.println(primero.getTipo());
+
+        System.out.println(primero.getUser());
+        primero.setUser("juanjoduran2003@hotmail.com");
+        System.out.println(primero.getUser());
+
+        System.out.println(primero.getValor());
+        primero.setValor(5000000);
+        System.out.println(primero.getValor());
     }
 }
