2ba0ead257c3842ad345dcb9051e35da48ab2be6
Deyber Jiménez <deyberantonio@gmail.com>
2022-09-28 23:30:37 -0500

    Sprint 4

--- proyecto/src/main/java/com/TheRavens/proyecto/Repositorios/MovimientoDineroRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.TheRavens.proyecto.Repositorios;
-
-import com.TheRavens.proyecto.Entidades.MovimientoDinero;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovimientoDineroRepositorio extends JpaRepository<MovimientoDinero, Long> {
-}
